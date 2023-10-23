78c8316b981811dbab227d5ce1896f2f0c322e3e
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-10-23 18:36:22 +0900

    普通の配列を受け取る関数に読み取り専用配列を渡せない
    読み取り専用配列は普通の配列の部分型ではないため

--- src/FunctionSubTypingByArgumentNumbersReadOnly.ts
+++ src/FunctionSubTypingByArgumentNumbersReadOnly.ts
@@ -7,3 +7,9 @@ console.log(sumAcceptsReadOnly(nums1)); // 6
 
 const nums2: number[] = [1, 2, 3];
 console.log(sumAcceptsReadOnly(nums2)); // 6
+
+function sumAcceptsWritable(nums: number[]): number {
+  return nums.reduce((a, b) => a + b, 0);
+}
+
+console.log(sumAcceptsWritable(nums2)); // 6
