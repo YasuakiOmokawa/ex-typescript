5ecb33ea7519dae8933383b71afc9e480453331f
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-10-30 19:07:50 +0900

    型引数と返却値の型を任意の型に指定できるようにした

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,4 +1,4 @@
-function customMap(array: number[], callback: Function): number[] {
+function customMap<T, Y>(array: T[], callback: Function): Y[] {
   const result = [];
   for (let i = 0; i < array.length; i++) {
     result.push(callback(array[i]));
@@ -6,6 +6,13 @@ function customMap(array: number[], callback: Function): number[] {
   return result;
 }
 
-const datas = [1, 2, 3, 4, 5];
+const datas: number[] = [1, 2, 3, 4, 5];
 const result = customMap(datas, (data: number) => data * 2);
 console.log(result); // [2, 4, 6, 8, 10]
+
+const datasChangeToBoolean: number[] = [1, -3, -2, 8, 0, -1];
+const resultBoolean: boolean[] = customMap(
+  datasChangeToBoolean,
+  (element: number) => element >= 0
+);
+console.log(resultBoolean); // [true, false, false, true, true, false]
