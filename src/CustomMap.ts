3c012980e4244ed134e6bc496c730b472429b911
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-10-30 19:11:19 +0900

    callback関数をFunctionインタフェースではなく、具体的な引数と返却値のリストにした

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,4 +1,4 @@
-function customMap<T, Y>(array: T[], callback: Function): Y[] {
+function customMap<T, Y>(array: T[], callback: (element: T) => Y): Y[] {
   const result = [];
   for (let i = 0; i < array.length; i++) {
     result.push(callback(array[i]));
@@ -7,12 +7,12 @@ function customMap<T, Y>(array: T[], callback: Function): Y[] {
 }
 
 const datas: number[] = [1, 2, 3, 4, 5];
-const result = customMap(datas, (data: number) => data * 2);
+const result = customMap(datas, (data) => data * 2);
 console.log(result); // [2, 4, 6, 8, 10]
 
 const datasChangeToBoolean: number[] = [1, -3, -2, 8, 0, -1];
 const resultBoolean: boolean[] = customMap(
   datasChangeToBoolean,
-  (element: number) => element >= 0
+  (element) => element >= 0
 );
 console.log(resultBoolean); // [true, false, false, true, true, false]
