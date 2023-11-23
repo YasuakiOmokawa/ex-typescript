4a3e4577417f0234774b1ece9ebc9fa4ed7d6482
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-11-23 16:10:29 +0900

    protectedの使い所
    deviseみたいに、継承を前提として、子クラスで独自のフックメソッドを定義したいときとかかな？

---
+++ src/InheritWithProtected.ts
@@ -0,0 +1,13 @@
+class NormalizeUser {
+  constructor(public name: string, protected age: number) {}
+
+  public isAdult() {
+    return this.age >= 20;
+  }
+}
+
+class LuxuriouslyUser extends NormalizeUser {
+  public override isAdult(): boolean {
+    return this.age >= 0;
+}
+
