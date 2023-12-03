cd32e7c3e00c02a88ae64dfea8451bb101dea56d
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-12-03 16:54:09 +0900

    クラスのなかのthisの呼ばれ方

--- src/ApplyThis.ts
+++ src/ApplyThis.ts
@@ -11,3 +11,14 @@ const applyJohn = new NormalUser2("John", 10);
 console.log(applyUhyo.isAdult()); // true
 
 console.log(applyUhyo.isAdult.apply(applyJohn, [])); // false
+
+class Athis {
+  foo = 123;
+  bar = this.foo + 100;
+  getFoo() {
+    return this.foo;
+  }
+}
+
+const aThis = new Athis();
+console.log(aThis.bar, aThis.getFoo()); // 223 123
