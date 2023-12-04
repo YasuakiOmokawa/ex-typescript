928a796b271ca77c6d779ad969eaaf4e3210749f
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-12-04 20:03:48 +0900

    エラーになるケースとならないケースを検証

--- src/TryError.ts
+++ src/TryError.ts
@@ -1,8 +1,17 @@
 console.log("エラーの検証をします");
-throwError();
+// throwError();
+console.log(getAverage([1, 2, 3]));
+console.log(getAverage([]));
 console.log("エラーの検証をしました");
 
 function throwError() {
   const error = new Error("エラーが発生しました");
   throw error;
+}
+
+function getAverage(nums: number[]) {
+  if (nums.length === 0) {
+    throw new Error("配列の要素が0です");
+  }
+  return nums.reduce((a, b) => a + b) / nums.length;
 }
