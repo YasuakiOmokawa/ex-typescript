003794ce39ae0feb0bb44970198c15d5a702d937
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-12-24 14:48:26 +0900

    インターセクション型について
    部分型であるらしい

---
+++ src/IntersectionType.ts
@@ -0,0 +1,16 @@
+type IntersectionAnimal = {
+  species: string;
+  age: number;
+};
+type IntersectionHuman = IntersectionAnimal & { name: string };
+
+const intersectionTama: IntersectionAnimal = {
+  species: "cat",
+  age: 10,
+};
+
+const intersectionOmo: IntersectionHuman = {
+  name: "omo",
+  species: "ホモサピエンス",
+  age: 38,
+};
