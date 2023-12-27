0e772abbdfd36ac3843e63c7d29f872a5fe3f74d
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-12-27 15:22:05 +0900

    オプショナルプロパティを定義すると、undefinedとのユニオン型になる
    という検証

---
+++ src/UnionOptionalType.ts
@@ -0,0 +1,13 @@
+type UnionOptionalHuman = {
+  name: string;
+  age?: number;
+};
+
+const omoIsOptionalHuman: UnionOptionalHuman = {
+  name: "omo",
+  age: 38,
+};
+
+const dadIsOptionalHuman: UnionOptionalHuman = {
+  name: "tama",
+};
