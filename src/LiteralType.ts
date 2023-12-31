4536576513dfd6b9b8bed9f75a619b0c4dc0d5df
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2023-12-31 16:42:01 +0900

    リテラル型のユニオン型を試す）

--- src/LiteralType.ts
+++ src/LiteralType.ts
@@ -1,3 +1,10 @@
 type FooString = "foo";
 
 const thisIsFooString: FooString = "foo";
+
+function signNumber(type: "plus" | "minus") {
+  return type === "plus" ? 1 : -1;
+}
+
+console.log(signNumber("plus")); // 1
+console.log(signNumber("minus")); // -1
