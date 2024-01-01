28635bd8883f6f99db1b81790c1dab3b5bd1c499
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-01-01 12:31:13 +0900

    オブジェクト型のwidening

--- src/LiteralTypeWidening.ts
+++ src/LiteralTypeWidening.ts
@@ -4,3 +4,18 @@ let hogeType2 = "hoge";
 let hogeType3: string | number = "hoge";
 hogeType3 = 1;
 let onlyMember: "omo" | "tama" = "tama";
+
+const wideningObject = {
+  name: "omo",
+  age: 38,
+};
+
+type notWideningType = {
+  readonly name: string;
+  readonly age: number;
+};
+
+const notWideningObject: notWideningType = {
+  name: "omo",
+  age: 38,
+};
