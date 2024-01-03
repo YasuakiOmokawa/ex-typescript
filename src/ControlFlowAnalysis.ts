e0fb229307c238dd28fedcd8fe03e00991ed6e11
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-01-03 17:01:44 +0900

    いかなる関数式を用いても、型の絞り込みは行われる
    等価式で絞り込みが行われた

--- src/ControlFlowAnalysis.ts
+++ src/ControlFlowAnalysis.ts
@@ -14,3 +14,18 @@ function numberWithSign(num: number, type: SignType | "none") {
 console.log(numberWithSign(5, "plus")); // 5
 console.log(numberWithSign(5, "minus")); // -5
 console.log(numberWithSign(5, "none"));
+
+function numberWithSign2(num: number, type: SignType | "none") {
+  if (type === "none") {
+    return 0;
+  }
+  return num * signNumber2(type);
+}
+
+console.log(numberWithSign2(5, "plus")); // 5
+
+function numberWithSign3(num: number, type: SignType | "none") {
+  return type === "none" ? 0 : num * signNumber2(type);
+}
+
+console.log(numberWithSign3(5, "plus")); // 5
