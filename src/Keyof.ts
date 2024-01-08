7392bc01c65308a98d2a3d72d6dc070d60dc0ace
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-01-08 13:17:00 +0900

    keyof typeofで引数を静的解析できるようにする
    引数の型を静的に規定することができる

--- src/Keyof.ts
+++ src/Keyof.ts
@@ -21,3 +21,20 @@ type LookofHumanKeys = keyof LookofHuman;
 
 let key: LookofHumanKeys = "type";
 key = "age";
+
+const mmConversionTable = {
+  mm: 1,
+  m: 1e3,
+  km: 1e6,
+};
+
+function convertUnits(value: number, unit: keyof typeof mmConversionTable) {
+  const mmValue = value * mmConversionTable[unit];
+  return {
+    mm: mmValue,
+    m: mmValue / 1e3,
+    km: mmValue / 1e6,
+  };
+}
+
+console.log(convertUnits(5600, "m"));
