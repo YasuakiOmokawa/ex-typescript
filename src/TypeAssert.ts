6709936b541fce8a431505c992da0473ec892036
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-01-20 14:22:22 +0900

    tsのコンパイルが不正確なときに、型アサーションを使う
    型安全が保証されているなら型アサーションを使ってよい

--- src/TypeAssert.ts
+++ src/TypeAssert.ts
@@ -1,7 +1,31 @@
-function getFirstFiveLetters(strOrNum: string | number) {
-  const str = strOrNum as string;
-  return str.slice(0, 5);
+type AnimalAssert = {
+  tag: "animal";
+  species: string;
+};
+type HumanAssert = {
+  tag: "human";
+  name: string;
+};
+type UserAssert = AnimalAssert | HumanAssert;
+
+function getNamesIfAllHuman(
+  users: readonly UserAssert[]
+): string[] | undefined {
+  if (users.every((user) => user.tag === "human")) {
+    return (users as HumanAssert[]).map((user) => user.name);
+  }
+  return undefined;
 }
 
-console.log(getFirstFiveLetters("TypeScript")); // TypeS
-console.log(getFirstFiveLetters(12345)); // 12345
+console.log(
+  getNamesIfAllHuman([
+    { tag: "human", name: "omo" },
+    { tag: "human", name: "tama" },
+  ])
+); // [ 'omo', 'tama' ]
+console.log(
+  getNamesIfAllHuman([
+    { tag: "human", name: "omo" },
+    { tag: "animal", species: "cat" },
+  ])
+); // undefined
