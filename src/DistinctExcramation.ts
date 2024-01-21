f769580fc3b97d56bd58219e3fc5d5bc0eddf010
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-01-21 09:58:25 +0900

    asや!を使わずに書く
    型安全を壊さずに書き直せるかをまず考えること

--- src/DistinctExcramation.ts
+++ src/DistinctExcramation.ts
@@ -7,13 +7,7 @@ function getOneUserName(
   user1?: DistinctHuman,
   user2?: DistinctHuman
 ): string | undefined {
-  if (user1 === undefined && user2 === undefined) {
-    return undefined;
-  }
-  if (user1 !== undefined) {
-    return user1.name;
-  }
-  return user2!.name;
+  return user1?.name ?? user2?.name;
 }
 
 console.log(getOneUserName({ tag: "human", name: "omo" })); // omo
