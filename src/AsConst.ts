960635b82c7ca4b7341091680e7c3eaed4a7745c
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-01-28 11:44:27 +0900

    as constから、型を生成する
    [number]は、配列インデックスのことを指しているっぽい。stringにするとエラーとなる。

--- src/AsConst.ts
+++ src/AsConst.ts
@@ -3,3 +3,6 @@ const names2 = ["Alice", "Bob", "Eve"] as const;
 
 names1.push("hoge"); // OK
 // names2.push("hoge"); // NG
+
+const namesConst = ["Alice", "Bob", "Eve"] as const;
+type NamesConstType = (typeof namesConst)[number];
