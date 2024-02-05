2bf5db38aac071b9555a34eb9165a01e26ed8ee8
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-02-05 17:44:14 +0900

    object型をインターセクションでつくれば、意図せずプリミティブ型を使われなくてもすむ

--- src/Never.ts
+++ src/Never.ts
@@ -13,3 +13,15 @@ useToString1({
 });
 
 useToString1(3.14);
+
+function useToString2(value: HasToString & object) {
+  console.log(`value is ${value.toString()}`);
+}
+
+useToString2({
+  toString() {
+    return "foo!";
+  },
+});
+
+// useToString2(3.14);
