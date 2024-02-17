b9de239affd9de8ba3f02f3d57c6598a2678bc76
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-02-17 20:57:38 +0900

    引数がユーザー定義型に該当するのか判定する

--- src/TypeGuards.ts
+++ src/TypeGuards.ts
@@ -13,3 +13,20 @@ function isStringOrNumberInvalid(value: unknown): value is string | number {
 }
 
 console.log(isStringOrNumberInvalid(something));
+
+type HumanGuard = {
+  type: "Human";
+  name: string;
+  age: number;
+};
+
+function isHuman(value: any): value is HumanGuard {
+  if (value == null) return false;
+  return (
+    value.type === "Human" &&
+    typeof value.name === "string" &&
+    typeof value.age === "number"
+  );
+}
+
+console.log(isHuman({ type: "Human", name: "Taro", age: 25 }));
