97b44249aaedca32eae604499a09970d95aad4dd
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-02-18 17:36:09 +0900

    assertで型ガード

--- src/TypeGuards.ts
+++ src/TypeGuards.ts
@@ -30,3 +30,21 @@ function isHuman(value: any): value is HumanGuard {
 }
 
 console.log(isHuman({ type: "Human", name: "Taro", age: 25 }));
+
+function assertHuman(value: any): asserts value is HumanGuard {
+  if (value == null) {
+    throw new Error("value is null or undefined");
+  }
+  if (
+    value.type !== "Human" ||
+    typeof value.name !== "string" ||
+    typeof value.age !== "number"
+  ) {
+    throw new Error("value is not a Human");
+  }
+}
+
+function checkAndUseHumanGuard(value: unknown) {
+  assertHuman(value);
+  console.log(value.name);
+}
