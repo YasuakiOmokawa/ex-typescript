7cf86500abf4dd217ad3184edba63833b318010f
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-02-23 15:31:02 +0900

    可変長タプル型の実用
    これどこで使うんだろ...？

--- src/VariadicTupleType.ts
+++ src/VariadicTupleType.ts
@@ -6,3 +6,11 @@ const arr2: NumberAndStrings = [2];
 type NumberStringNumber = [number, ...string[], number];
 const arr3: NumberStringNumber = [1, "hello", "world", 2];
 const arr4: NumberStringNumber = [2, 3];
+
+type AddMax<T extends unknown[]> = [max: number, ...rest: T];
+//          ^ ジェネリクスでTをタプルに制限しています
+//                                                ^ ... でどこにマージするかを示しています
+
+// これは次のように型を合成することができます:
+type MaxMin = AddMax<[min: number]>;
+type MaxMinDiameter = AddMax<[min: number, diameter: number]>;
