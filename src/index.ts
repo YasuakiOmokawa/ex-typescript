52c689f0160a22c065592cdf31cf2505487aff32
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2024-03-09 14:25:12 +0900

    クロージャーとしての関数export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-const message: string = 'Hello World';
+import { increment } from "./counter.js";
+const message: string = "Hello World";
 
 console.log(message);
+console.log(`increment value: ${increment()}`);
+console.log(`increment value: ${increment()}`);
+console.log(`increment value: ${increment()}`);
