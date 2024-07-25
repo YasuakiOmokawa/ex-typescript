3d30b29e6fe7eea281e5051b421df06e460d1da4
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-25 14:32:52 +0900

    add perf_hooks on reading file

--- src/Async2.ts
+++ src/Async2.ts
@@ -1,6 +1,9 @@
 import { readFile } from "fs";
+import { performance } from "perf_hooks";
 
-readFile("foo.txt", "utf-8", (_err, result) => {
-  console.log(result);
+const startTime = performance.now();
+readFile("foo.txt", "utf-8", (_err, _result) => {
+  const endTime = performance.now();
+  console.log(`elapsed ${endTime - startTime} miliseconds`);
 });
 console.log("start reading");
