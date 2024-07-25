9f94ea91af7e02d5e816ad693b7291838498126d
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-25 14:52:43 +0900

    add counter to async1
    not to interrupt asynchronous doings into synchronouses.

--- src/Async1.ts
+++ src/Async1.ts
@@ -1,4 +1,12 @@
+import { performance } from "perf_hooks";
+
 setTimeout(() => {
   console.log("called timer!");
-}, 3000);
-console.log("setteled timer!");
+}, 100);
+
+const startTime = performance.now();
+let count = 0;
+while (performance.now() - startTime < 1000) {
+  count++;
+}
+console.log(count);
