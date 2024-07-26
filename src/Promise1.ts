0e677fa5fd5c5837951cfbd6b54ccb76d89a3665
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-26 16:33:03 +0900

    add stdin interaction

--- src/Promise1.ts
+++ src/Promise1.ts
@@ -1,12 +1,23 @@
+import { createInterface } from "readline/promises";
 import { readFile } from "fs/promises";
 
-const p = readFile("foo2.txt", "utf-8");
-
-p.then((result) => {
-  console.log("success!");
-  console.table(result);
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
-p.catch((error) => {
-  console.log("error!");
-  console.table(error);
+
+const promisedRl = rl.question("please input file name: ");
+promisedRl.then((line) => {
+  const promisedRf = readFile(line, "utf-8");
+  promisedRf.then((result) => {
+    console.log("success!");
+    console.table(result);
+  });
+  promisedRf.catch((error) => {
+    console.log("error!");
+    console.table(error);
+  });
+});
+promisedRl.finally(() => {
+  rl.close();
 });
