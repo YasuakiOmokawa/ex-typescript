16435aa9893a11e1d16ceb79480f1f567dd09037
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-26 16:46:58 +0900

    add catch to stdin interaction

--- src/Promise1.ts
+++ src/Promise1.ts
@@ -10,14 +10,19 @@ const promisedRl = rl.question("please input file name: ");
 promisedRl.then((line) => {
   const promisedRf = readFile(line, "utf-8");
   promisedRf.then((result) => {
-    console.log("success!");
+    console.log("success read file!");
     console.table(result);
   });
   promisedRf.catch((error) => {
-    console.log("error!");
+    console.log("error read file!");
     console.table(error);
   });
 });
+promisedRl.catch((error) => {
+  console.log("error read line interface.");
+  console.table(error);
+});
 promisedRl.finally(() => {
   rl.close();
+  console.log("closes read line interface.");
 });
