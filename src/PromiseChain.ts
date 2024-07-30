2910cd181839e92e1113c26c7c3f94069ca01599
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 16:23:43 +0900

    mod to dynamic import

--- src/PromiseChain.ts
+++ src/PromiseChain.ts
@@ -1,12 +1,11 @@
-import { readFile } from "fs/promises";
-
 const repeat10 = (str: string) => {
   return new Promise<string>((resolve) => {
     setTimeout(() => resolve(str.repeat(10)), 100);
   });
 };
 
-readFile("foo.txt", "utf8")
+import("fs/promises")
+  .then(({ readFile }) => readFile("foo.txt", "utf8"))
   .then((result) => repeat10(result))
   .then((result) => {
     console.log(result);
