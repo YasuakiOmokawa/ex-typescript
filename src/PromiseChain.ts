6d7005dd19cb04cc8a5e890bc6054cb9aa55cdb2
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 15:43:47 +0900

    add chain asyncs

--- src/PromiseChain.ts
+++ src/PromiseChain.ts
@@ -1,7 +1,13 @@
 import { readFile } from "fs/promises";
 
-readFile("fo.txt", "utf8")
-  .catch(() => "")
+const repeat10 = (str: string) => {
+  return new Promise<string>((resolve) => {
+    setTimeout(() => resolve(str.repeat(10)), 100);
+  });
+};
+
+readFile("foo.txt", "utf8")
+  .then((result) => repeat10(result))
   .then((result) => {
     console.log(result);
   });
