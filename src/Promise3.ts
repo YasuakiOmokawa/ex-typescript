b0c4ea58fbcbaa1b5bd298a98948452176b1995b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 12:33:55 +0900

    promise does after syncs

--- src/Promise3.ts
+++ src/Promise3.ts
@@ -1,21 +1,27 @@
 const p = Promise.resolve(100);
 
-p.then((result) => {
-  console.log(`result is ${result}`);
-});
+// p.then((result) => {
+//   console.log(`result is ${result}`);
+// });
 
 const p2 = new Promise((resolve) => {
   resolve(100);
 });
 
-p2.then((result) => {
-  console.log(`result2 is ${result}`);
-});
+// p2.then((result) => {
+//   console.log(`result2 is ${result}`);
+// });
 
-const pR = Promise.reject("dameyo");
+// const pR = Promise.reject("dameyo");
 // const pR = new Promise((resolve, reject) => {
 //   reject("dame-yo!");
 // });
-pR.catch((error) => {
-  console.log(`error is ${error}`);
+// pR.catch((error) => {
+//   console.log(`error is ${error}`);
+// });
+
+const pSecond = Promise.resolve();
+p.then(() => {
+  console.log(2);
 });
+console.log(1);
