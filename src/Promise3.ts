5a13d4d8ecdd37b3eaf0621e56dd67318f368298
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 12:29:10 +0900

    add error-cased function

--- src/Promise3.ts
+++ src/Promise3.ts
@@ -11,3 +11,11 @@ const p2 = new Promise((resolve) => {
 p2.then((result) => {
   console.log(`result2 is ${result}`);
 });
+
+const pR = Promise.reject("dameyo");
+// const pR = new Promise((resolve, reject) => {
+//   reject("dame-yo!");
+// });
+pR.catch((error) => {
+  console.log(`error is ${error}`);
+});
