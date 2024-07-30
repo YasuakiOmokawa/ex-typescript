5620c7128940a4728c2cbe75f945ea9b132d1030
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 14:31:18 +0900

    impl PromiseRace

---
+++ src/PromiseRace.ts
@@ -0,0 +1,10 @@
+import { readFile } from "fs/promises";
+
+const p = Promise.race([
+  readFile("foo.txt", "utf8"),
+  readFile("bar.txt", "utf8"),
+]);
+
+p.then((result) => {
+  console.log(`result file: ${result}`);
+});
