61885636d3abe90299d1efbc02f3da15dbe8c99d
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 12:07:31 +0900

    add sleepReject

--- src/Promise2.ts
+++ src/Promise2.ts
@@ -7,3 +7,13 @@ const sleep = (duration: number) => {
 sleep(200).then(() => {
   console.log(`elapsed 0.2 secs.`);
 });
+
+const sleepReject = (duration: number) => {
+  return new Promise<never>((_resolve, reject) => {
+    setTimeout(reject, duration);
+  });
+};
+
+sleepReject(200).catch(() => {
+  console.log("failed!");
+});
