c8bb0d42f138a5720c83e2229bddfd86451149df
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-30 16:15:05 +0900

    refactor: chain read file promises

--- src/Promise1.ts
+++ src/Promise1.ts
@@ -8,15 +8,15 @@ const rl = createInterface({
 
 const promisedRl = rl.question("please input file name: ");
 promisedRl.then((line) => {
-  const promisedRf = readFile(line, "utf-8");
-  const fileReading = promisedRf.then((result) => {
-    console.log("success read file!");
-    console.table(result);
-  });
-  fileReading.catch((error: unknown) => {
-    console.log("error read file!");
-    console.table(error);
-  });
+  readFile(line, "utf-8")
+    .then((result) => {
+      console.log("success read file!");
+      console.table(result);
+    })
+    .catch((error: unknown) => {
+      console.log("error read file!");
+      console.table(error);
+    });
 });
 promisedRl.catch((error: unknown) => {
   console.log("error read line interface.");
