779aa14467ae070545b3244b25934ef36cdf0034
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-31 14:59:06 +0900

    mod: using await

--- src/Await3.ts
+++ src/Await3.ts
@@ -1,16 +1,16 @@
-import("fs/promises")
-  .then(({ readFile }) => readFile("uhyo.txt", "utf8"))
-  .then((data) => {
-    let count = 0;
-    let currentIndex = 0;
-    while (true) {
-      const nextIndex = data.indexOf("uhyo", currentIndex);
-      if (nextIndex >= 0) {
-        count++;
-        currentIndex = nextIndex + 1;
-      } else {
-        break;
-      }
-    }
-    console.log(count);
-  });
+import { readFile } from "fs/promises";
+
+const data = await readFile("uhyo.txt", "utf8");
+
+let count = 0;
+let currentIndex = 0;
+while (true) {
+  const nextIndex = data.indexOf("uhyo", currentIndex);
+  if (nextIndex >= 0) {
+    count++;
+    currentIndex = nextIndex + 1;
+  } else {
+    break;
+  }
+}
+console.log(count);
