815185897c890939180f46528274b5427918560b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-31 15:43:26 +0900

    mod: catch on file read

--- src/Await3.ts
+++ src/Await3.ts
@@ -2,27 +2,33 @@ import { readFile } from "fs/promises";
 import Timeout from "await-timeout";
 
 const timer = new Timeout();
-let data: string = "";
 try {
-  data = await Promise.race([
+  const data = await Promise.race([
     readFile("uhyo.txt", "utf8"),
-    timer.set(1000, "TimeOut!"),
-  ]);
-} catch (error) {
-  console.log("file read error: ", error);
+    timer.set(0, "TimeOut!"),
+  ]).catch((error) => {
+    console.log(
+      "file read error! process continue on empty string data. error details: ",
+      error
+    );
+    return "";
+  });
+  countUhyo(data);
 } finally {
   timer.clear();
 }
 
-let count = 0;
-let currentIndex = 0;
-while (true) {
-  const nextIndex = data.indexOf("uhyo", currentIndex);
-  if (nextIndex >= 0) {
-    count++;
-    currentIndex = nextIndex + 1;
-  } else {
-    break;
+function countUhyo(data: string) {
+  let count = 0;
+  let currentIndex = 0;
+  while (true) {
+    const nextIndex = data.indexOf("uhyo", currentIndex);
+    if (nextIndex >= 0) {
+      count++;
+      currentIndex = nextIndex + 1;
+    } else {
+      break;
+    }
   }
+  console.log(count);
 }
-console.log(count);
