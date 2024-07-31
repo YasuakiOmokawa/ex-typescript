e1d729dd5e3cfa5507abfbf50d8c6841eaaf90ba
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-31 14:36:53 +0900

    catch fail pattern

--- src/Await2.ts
+++ src/Await2.ts
@@ -1,9 +1,13 @@
 async function main2() {
   const { readFile, writeFile } = await import("fs/promises");
 
-  const fooContent = await readFile("foo.txt", "utf-8");
-  await writeFile("bar.txt", fooContent.repeat(2));
-  console.log("end write file");
+  try {
+    const fooContent = await readFile("foao.txt", "utf-8");
+    await writeFile("bar.txt", fooContent.repeat(2));
+    console.log("end write file");
+  } catch {
+    console.error("this is fail");
+  }
 }
 
 main2().then(() => {
