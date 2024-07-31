6487358944e44332d867dec867c6589b3769c5f4
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-31 13:34:07 +0900

    note calling process on async

--- src/Async3.ts
+++ src/Async3.ts
@@ -1,8 +1,11 @@
 async function get3(): Promise<number> {
+  console.log("have called get3()");
   return 3;
 }
 
+console.log("begin to call get3()");
 const pGet3 = get3();
 pGet3.then((num) => {
   console.log(`num: ${num}`);
 });
+console.log("called get3()");
