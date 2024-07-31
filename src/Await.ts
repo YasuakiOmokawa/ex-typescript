50890209168aa59bb372e6332d2fc3a47b4c30de
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-07-31 14:01:23 +0900

    notes await process doing

--- src/Await.ts
+++ src/Await.ts
@@ -5,11 +5,15 @@ const sleepForAwait = (duration: number) => {
 };
 
 async function get3Await() {
+  console.log("have called get3 await");
   await sleepForAwait(1000);
+  console.log("steps after await");
   return 3;
 }
 
+console.log("start call get3 await");
 const pAwait = get3Await();
 pAwait.then((num) => {
   console.log(`num: ${num}`);
 });
+console.log("called get3 await");
