4ea4f25329ea2130ad17766d5a971d03f5005223
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-01 14:34:18 +0900

    構造的部分型

--- src/Union1.ts
+++ src/Union1.ts
@@ -17,3 +17,11 @@ const omoSan: ForUnionUser = {
 function getUserNameDaze(user: ForUnionUser): string {
   return user.name;
 }
+
+const tamaSan2 = {
+  species: "john doe species",
+  specialProp1: "hoge",
+};
+
+const animalSan: ForUnionAnimal = tamaSan2;
+animalSan.species; // enable access so that type-defined.
