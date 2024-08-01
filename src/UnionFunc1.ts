bf68856b78231f47513c3a35593f842b5fc2ca56
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-01 15:15:07 +0900

    move UnionFunc1

---
+++ src/UnionFunc1.ts
@@ -0,0 +1,21 @@
+namespace Union {
+  export type Human = { name: string };
+  export type Animal = { species: string };
+}
+
+function getBeingName(human: Union.Human) {
+  return human.name;
+}
+function getBeingSpecies(animal: Union.Animal) {
+  return animal.species;
+}
+
+const mysteryFuncDesuyo = Math.random() < 0.5 ? getBeingName : getBeingSpecies;
+
+const omoIntersection: Union.Human & Union.Animal = {
+  name: "omokawa",
+  species: "human being",
+};
+
+const value = mysteryFuncDesuyo(omoIntersection);
+console.log(value);
