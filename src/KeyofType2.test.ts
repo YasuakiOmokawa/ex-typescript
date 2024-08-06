72ce05698a7d00b40114cb5ab0336f5068f83f42
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-06 12:10:57 +0900

    impl KeyofType2.test.ts

---
+++ src/KeyofType2.test.ts
@@ -0,0 +1,9 @@
+import { convertUnits } from "./KeyofType2";
+
+test("対象のユニットの割合に変換されること", () => {
+  expect(convertUnits(5600, "m")).toMatchObject({
+    mm: 5600000,
+    m: 5600,
+    km: 5.6,
+  });
+});
