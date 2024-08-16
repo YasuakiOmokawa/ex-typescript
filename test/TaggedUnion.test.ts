730a0ff66dca4e613e3700ae1f80933352a14e7b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-16 18:11:11 +0900

    complete test case

--- test/TaggedUnion.test.ts
+++ test/TaggedUnion.test.ts
@@ -2,11 +2,29 @@ import { getStatus } from "../src/TaggedUnion";
 import { Common } from "../src/commonTypes";
 
 describe("getStatus()", () => {
-  const success: Common.Success = {
-    done: true,
-  };
-
   it("success", () => {
+    const success: Common.Success = {
+      done: true,
+    };
+
     expect(getStatus(success)).toBe("upload success");
+  });
+
+  it("failure", () => {
+    const failure: Common.Failure = {
+      done: true,
+      error: Error("this is error"),
+    };
+
+    expect(getStatus(failure)).toBe("upload failed: this is error");
+  });
+
+  it("inprogress", () => {
+    const inprogress: Common.Inprogress = {
+      done: false,
+      progress: 55,
+    };
+
+    expect(getStatus(inprogress)).toBe("now uploading: 55%");
   });
 });
