7550a5906b9d160a1ff67e4ea47a224483c957a6
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-19 12:14:49 +0900

    change statement switch

--- src/TaggedUnion.ts
+++ src/TaggedUnion.ts
@@ -1,13 +1,14 @@
 import { Common } from "./commonTypes";
 
 export function getStatus(status: Common.UploadStatus) {
-  if (status.type === "Failure") {
-    return `upload failed: ${status.error.message}`;
-  } else if (status.type === "Success") {
-    return `upload success`;
-  } else if (status.type === "InProgress") {
-    return `now uploading: ${status.progress}%`;
-  } else {
-    return `unknown status: ${status}`;
+  switch (status.type) {
+    case "Failure":
+      return `upload failed: ${status.error.message}`;
+    case "Success":
+      return `upload success`;
+    case "InProgress":
+      return `now uploading: ${status.progress}%`;
+    default:
+      return `unknown status: ${status}`;
   }
 }
