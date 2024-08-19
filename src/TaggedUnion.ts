ae786508dac51483caa4e270cc3446dbb3a95883
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2024-08-19 12:11:53 +0900

    refactor: add tagged union

--- src/TaggedUnion.ts
+++ src/TaggedUnion.ts
@@ -1,13 +1,13 @@
 import { Common } from "./commonTypes";
 
 export function getStatus(status: Common.UploadStatus) {
-  if (status.done) {
-    if ("error" in status) {
-      return `upload failed: ${status.error.message}`;
-    } else {
-      return `upload success`;
-    }
-  } else if ("progress" in status) {
+  if (status.type === "Failure") {
+    return `upload failed: ${status.error.message}`;
+  } else if (status.type === "Success") {
+    return `upload success`;
+  } else if (status.type === "InProgress") {
     return `now uploading: ${status.progress}%`;
+  } else {
+    return `unknown status: ${status}`;
   }
 }
