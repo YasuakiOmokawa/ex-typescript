a0512921d5b36b1db76eca23b4030334e379fe59
Yasuaki Omokawa <omokawa@senk-inc.co.jp>
2025-04-25 13:23:44 +0900

    impl extent

---
+++ src/extent.ts
@@ -0,0 +1,15 @@
+function extent(nums: Iterable<number>) {
+  let min, max;
+  for (const num of nums) {
+    if (!min) {
+      min = num;
+      max = num;
+    } else {
+      min = Math.min(min, num);
+      max = Math.max(max, num);
+    }
+  }
+  return [min, max];
+}
+
+const [min, max] = extent([0, 1, 2]);
